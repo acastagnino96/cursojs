489821fcc1c1cf701dc90ae2c63d8678bfe77543
agus <acastagnino@hotmail.com>
2022-09-28 19:57:32 -0300

    Interaccion con JSON y Storage

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 class producto{
     constructor (id, nombre, tipo, precio, stock, img){
         this.id = id;
-        this.nombre = nombre;
+        this.nombre = nombre.toUpperCase();
         this.tipo = tipo
-        this.precio = precio;
+        this.precio = parseFloat(precio);
         this.stock = stock;
         this.img = img;
     }
@@ -26,42 +26,55 @@ productos.push(lemonPie, cremeBrulee, cremeDeCoco, crumbleManzana, perasYChocola
 
 const cards = document.getElementById("cards");
 
-const carrito = []
+let carrito = []
 
-for (const producto of productos) {
-    let column = document.createElement("div");
-    column.className = "col-md-4 mt-3 ";
-    column.id = `columna-${producto.id}`;
-    column.innerHTML = `
-        <div class="card">
-            <div class="card-body">
-            <img class="w-50" src= "${producto.img}" alt="producto">
-            <p class="card-text"><b>${producto.nombre}</b></p>
-            <p class="card-text">Tipo: <b>${producto.tipo}</b></p>
-            <p class="card-text">Precio: <b>${producto.precio}</b></p>
-            <div class="card-footer">
-                    <button onclick="guardarEnCarrito(${producto.id})" class="btn btn-primary" id="agregar-${producto.id}" >Agregar al carrito</button>
+function mostrarProductos(productos) {
+    cards.innerHTML= ""
+    productos.forEach(producto => {
+        let column = document.createElement("div");
+        column.className = "col-md-4 mt-3 ";
+        column.id = `columna-${producto.id}`;
+        column.innerHTML = `
+            <div class="card">
+                <div class="card-body">
+                <img class="w-50" src= "${producto.img}" alt="producto">
+                <p class="card-text"><b>${producto.nombre}</b></p>
+                <p class="card-text">Tipo: <b>${producto.tipo}</b></p>
+                <p class="card-text">Precio: <b>${producto.precio}</b></p>
+                <div class="card-footer">
+                        <button onclick="guardarEnCarrito(${producto.id})" class="btn btn-primary" id="agregar-${producto.id}" >Agregar al carrito</button>
+                    </div>
                 </div>
-            </div>
-        </div>`;
-  
-    cards.append(column);
+            </div>`;
+    
+        cards.append(column);
+    });
+}
 
-  }
+//// Carrito de compras ////
 
-  function guardarEnCarrito(comidaId){
+function guardarEnCarrito(comidaId){
     let item = productos.find((comida) => comida.id === comidaId)
     carrito.push(item)
     console.log (carrito)
+    guardarCarritoEnLocalStorage()
     renderCarrito()
     calcularTotal()
 }
 
 const contenedor = document.getElementById("carrito");
 
-const renderCarrito = () => {
+function renderCarrito() {
     contenedor.innerHTML = ""
-    carrito.forEach((item) => {
+    let carritoSinDuplicados = [...new Set(carrito)];
+    carritoSinDuplicados.forEach((item) => {
+        const miItem = productos.filter((prod) => {
+            return prod.idprod === parseInt(item);
+        });
+        const unidadesProd = carrito.reduce((total, itemId) => {
+            return itemId === item ? total += 1 : total;
+        }, 0);
+
         let div = document.createElement("div")
         div.className = "col-md-4 mt-3 ";
         div.id = `columna-${item.id}`;
@@ -71,6 +84,7 @@ const renderCarrito = () => {
             <img class="w-50" src= "${item.img}" alt="producto">
             <p class="card-text"><b>${item.nombre}</b></p>
             <p class="card-text">Tipo: <b>${item.tipo}</b></p>
+            <p class="card-text">Cantidad: <b>${unidadesProd}</b></p>
             <p class="card-text">Precio: <b>${item.precio}</b></p>
         </div>
         <button onclick="eliminarItem(${item.id})" class="btn btn-primary">Eliminar</button>`;
@@ -83,13 +97,31 @@ const eliminarItem = (id) => {
     let borrar = carrito.find((comida) => comida.id === id)
     let indice = carrito.indexOf(borrar)
     carrito.splice(indice, 1)
+    guardarCarritoEnLocalStorage()
     renderCarrito()
     calcularTotal()
 }
 
+//// Local Storage ////
+
+function guardarCarritoEnLocalStorage() {
+    let miLocalStorage = JSON.stringify(carrito);
+    localStorage.setItem("carrito", miLocalStorage);
+}
+
+function cargarCarritoDeLocalStorage() {
+    let miLocalStorage = localStorage.getItem("carrito");
+    if (miLocalStorage) {
+        carrito = JSON.parse(miLocalStorage);
+        renderCarrito();
+    }
+}
+
+//// Calculo del total ////
+
 const divPrecio = document.getElementById("precioTotal"); 
 
-calcularTotal = () => {
+function calcularTotal () {
     let cont = 0
     carrito.forEach((pre) => {
         cont += pre.precio
@@ -97,3 +129,11 @@ calcularTotal = () => {
 
     divPrecio.innerHTML = cont
 }
+
+function main(){
+    mostrarProductos(productos)
+    cargarCarritoDeLocalStorage()
+    calcularTotal ()
+}
+
+main();
