27a6628bfd8e6714f6a6f44451d856d85c08c906
agus <acastagnino@hotmail.com>
2022-10-03 21:20:52 -0300

    agregado de toastify

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-class producto{
+class Producto{
     constructor (id, nombre, tipo, precio, stock, img){
         this.id = id;
         this.nombre = nombre.toUpperCase();
@@ -9,16 +9,16 @@ class producto{
     }
 }
 
-const lemonPie = new producto(1, "Lemon Pie", "Tarteleta", 700, 10, "./images/lemonpie.jpg")
-const cremeBrulee = new producto(2, "Creme Brulee", "Tarteleta", 700, 10, "./images/cremebrulee.jpg")
-const cremeDeCoco = new producto(3, "Creme de Coco", "Tarteleta", 700, 10, "./images/cremedecoco.jpg")
-const crumbleManzana = new producto(4, "Crumble de Manzana", "Tarteleta", 700, 10, "./images/crumblemanzana.jpg")
-const perasYChocolate = new producto(5, "Peras y Chocolate", "Tarteleta", 700, 10, "./images/peras.jpg")
-const frutosRojos = new producto(6, "Frutos Rojos", "Tarteleta", 700, 10, "./images/frutosrojos.jpg")
-const nuecesYDDL = new producto(7, "Nueces y Dulce de Leche", "Tarteleta", 700, 10, "./images/nuecesddl.jpg")
-const chocolatePasion = new producto(8, "Chocolate Pasión", "Tarteleta", 700, 10, "./images/chocolate.jpg")
-const bombonesDDL = new producto(11, "Bombones de Dulce de Leche", "Bombones", 600, 10, "./images/bombones.jpg")
-const mentitas = new producto(12, "Mentitas", "Bombones", 600, 10, "./images/mentitas.jpg")
+const lemonPie = new Producto(1, "Lemon Pie", "Tarteleta", 700, 10, "./images/lemonpie.jpg")
+const cremeBrulee = new Producto(2, "Creme Brulee", "Tarteleta", 700, 10, "./images/cremebrulee.jpg")
+const cremeDeCoco = new Producto(3, "Creme de Coco", "Tarteleta", 700, 10, "./images/cremedecoco.jpg")
+const crumbleManzana = new Producto(4, "Crumble de Manzana", "Tarteleta", 700, 10, "./images/crumblemanzana.jpg")
+const perasYChocolate = new Producto(5, "Peras y Chocolate", "Tarteleta", 700, 10, "./images/peras.jpg")
+const frutosRojos = new Producto(6, "Frutos Rojos", "Tarteleta", 700, 10, "./images/frutosrojos.jpg")
+const nuecesYDDL = new Producto(7, "Nueces y Dulce de Leche", "Tarteleta", 700, 10, "./images/nuecesddl.jpg")
+const chocolatePasion = new Producto(8, "Chocolate Pasión", "Tarteleta", 700, 10, "./images/chocolate.jpg")
+const bombonesDDL = new Producto(11, "Bombones de Dulce de Leche", "Bombones", 600, 10, "./images/bombones.jpg")
+const mentitas = new Producto(12, "Mentitas", "Bombones", 600, 10, "./images/mentitas.jpg")
 
 const productos = []
 
@@ -30,19 +30,19 @@ let carrito = []
 
 function mostrarProductos(productos) {
     cards.innerHTML= ""
-    productos.forEach(producto => {
+    productos.forEach(Producto => {
         let column = document.createElement("div");
         column.className = "col-md-4 mt-3 ";
-        column.id = `columna-${producto.id}`;
+        column.id = `columna-${Producto.id}`;
         column.innerHTML = `
             <div class="card">
                 <div class="card-body">
-                <img class="w-50" src= "${producto.img}" alt="producto">
-                <p class="card-text"><b>${producto.nombre}</b></p>
-                <p class="card-text">Tipo: <b>${producto.tipo}</b></p>
-                <p class="card-text">Precio: <b>${producto.precio}</b></p>
+                <img class="w-50" src= "${Producto.img}" alt="producto">
+                <p class="card-text"><b>${Producto.nombre}</b></p>
+                <p class="card-text">Tipo: <b>${Producto.tipo}</b></p>
+                <p class="card-text">Precio: <b>${Producto.precio}</b></p>
                 <div class="card-footer">
-                        <button onclick="guardarEnCarrito(${producto.id})" class="btn btn-primary" id="agregar-${producto.id}" >Agregar al carrito</button>
+                        <button onclick="guardarEnCarrito(${Producto.id})" class="btn btn-primary" id="agregar-${Producto.id}" >Agregar al carrito</button>
                     </div>
                 </div>
             </div>`;
@@ -55,6 +55,7 @@ function mostrarProductos(productos) {
 
 function guardarEnCarrito(comidaId){
     let item = productos.find((comida) => comida.id === comidaId)
+    mostrarToast(item);
     carrito.push(item)
     console.log (carrito)
     guardarCarritoEnLocalStorage()
@@ -100,6 +101,20 @@ const eliminarItem = (id) => {
     guardarCarritoEnLocalStorage()
     renderCarrito()
     calcularTotal()
+}
+
+//// Mostrar Toast ////
+
+function mostrarToast(item) {
+    Toastify({
+        text: `PRODUCTO AGREGADO:
+                ${item.nombre}`,
+        duration: 2000,
+        className: "toastAgregar",
+        offset: {
+            y: 80
+        },
+    }).showToast();
 }
 
 //// Local Storage ////
